84d54c67faeef9177bcf05c0bbc65201c8026aa6
Mohammed Harris <md.harry.98@gmail.com>
2023-08-19 19:11:12 -0400

    building banner component with react carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import Image from 'next/image'
 import Header from '@/components/Header'
+import Head from 'next/head'
+import Banner from '@/components/Banner'
 
 export default function Home() {
   return (
+    <div>
+    <Head>
+      <title>Amazon 2.0 (Clone)</title>
+    </Head>
 
-    <main>
     <Header />
+
+    <main className='max-w-screen-2xl '>
+
+    {/* Banner */}
+    <Banner />
+
+    {/* Product Feed  */}
+    
     </main>
+    </div>
   )
 }
