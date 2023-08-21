5a8e2dd013aa73a535be6c8b0fbbfd4901e85f9c
Mohammed Harris <md.harry.98@gmail.com>
2023-08-20 22:26:27 -0400

    Product grid

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,8 +3,7 @@ import Product from './Product';
 
 function ProductFeed({ products }) {
   return (
-    <div>
-      <h1>Products here...</h1>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {products.map(({ id, title, price, description, category, image }) => (
         <Product key={id} title={title} price={price} description={description} category={category} image={image}/>
       ))}
